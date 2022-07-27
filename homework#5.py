242739a7dc65947a8845b5f628ae665a768b5452
DanilaPonezda <109663267+DanilaPonezda@users.noreply.github.com>
2022-07-28 00:31:53 +0300

    Add files via upload

--- homework#5.py
+++ homework#5.py
@@ -1,8 +1,21 @@
-spisok = ("Daniil", (25, "hello"), True, "hello")
+import re
 
+def validate():
+    while True:
+        password = input("Введите ваш пароль: ")
+        if len(password) < 8:
+            print("Напишите 8 символов в вашем пароле")
+        elif re.search('[0-9]',password) is None:
+            print("Напишите хотя бы одну цфиру в вашем пароле")
+        elif re.search('[A-Z]',password) is None: 
+            print("Напишите хотя бы одну большую букву в вашем пароле")
+        elif re.search('[a-z]',password) is None: 
+            print("Напишите хотя бы одну маленькую букву в вашем пароле в вашем пароле")
+        else:
+            print("ваш пароль надёжный")
+            break
 
-for i in spisok:
-    print(i)
+validate()
     
 
 
